847a4872c896546488f4222d89740737ca7ea9ae
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2024-02-19 01:26:08 +0530

    auth working

--- main.go
+++ main.go
@@ -8,21 +8,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type GoUser struct {
+	Username string `json:"username" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=8"`
+}
+
 var (
-	validUsername = "goUser"
-	validPassword = "goPassword"
+	users []GoUser
 )
 
 func main() {
 	router := gin.Default()
 
-	// Enable CORS
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:5173"}
 	router.Use(cors.New(config))
 
 	router.GET("/", goGinHello)
 	router.POST("/login", goGinLogin)
+	router.POST("/register", goGinRegister)
+
+	dummyUsers := []GoUser{
+		{Username: "user1", Email: "user1@example.com", Password: "password1"},
+		{Username: "user2", Email: "user2@example.com", Password: "password2"},
+		// Add more dummy users as needed
+	}
+
+	// Append dummy users to the existing users slice
+	users = append(users, dummyUsers...)
 
 	err := router.Run("localhost:9090")
 	if err != nil {
@@ -45,9 +59,32 @@ func goGinLogin(c *gin.Context) {
 		return
 	}
 
-	if loginData.Username == validUsername && loginData.Password == validPassword {
-		c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
-	} else {
-		c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials"})
+	for _, user := range users {
+		if user.Username == loginData.Username && user.Password == loginData.Password {
+			c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
+			return
+		}
 	}
+
+	c.JSON(http.StatusUnauthorized, gin.H{"message": "Invalid credentials"})
+}
+
+func goGinRegister(c *gin.Context) {
+	var registerData GoUser
+
+	if err := c.ShouldBindJSON(&registerData); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	for _, user := range users {
+		if user.Username == registerData.Username {
+			c.JSON(http.StatusConflict, gin.H{"message": "Username already taken"})
+			return
+		}
+	}
+
+	users = append(users, registerData)
+
+	c.JSON(http.StatusOK, gin.H{"message": "User registered successfully"})
 }
